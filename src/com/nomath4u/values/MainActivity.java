95bb82f2717a2379dc8b9a6d561b14b7ff0b5423
Chris Harper <nomath4u@gmail.com>
2012-12-11 21:26:56 -0800

    Accelerometer working

--- src/com/nomath4u/values/MainActivity.java
+++ src/com/nomath4u/values/MainActivity.java
@@ -3,6 +3,7 @@ package com.nomath4u.values;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.TextView;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -12,17 +13,22 @@ public class MainActivity extends Activity {
 	
 	private SensorManager mSensorManager;
 	private Sensor mAccelerometer;
-	private SensorEventListener mListener;
+	private SensorEventListener mAccelerometerListener;
+	private TextView mTextViewAccelerometer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
+        mTextViewAccelerometer = (TextView) findViewById(R.id.text_accelerometer);
+        
+        initListeners();
+        
         // Get an instance of the SensorManager
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        mSensorManager.registerListener(mListener, mAccelerometer,SensorManager.SENSOR_DELAY_UI);
+        mSensorManager.registerListener(mAccelerometerListener, mAccelerometer,SensorManager.SENSOR_DELAY_UI);
     }
 
     @Override
@@ -32,7 +38,18 @@ public class MainActivity extends Activity {
         return true;
     }
     
-    @Override
-    public void onSensorChanged(SensorEvent event){
-    } 
+    private void initListeners(){
+    	mAccelerometerListener = new SensorEventListener(){
+    		@Override
+    		public void onSensorChanged(SensorEvent event){
+    			float[] values = event.values;
+    			mTextViewAccelerometer.setText("Accelerometer: " + values[0] + ", " + values [1] + ", " + values[2]);
+    		}
+    		
+    		@Override
+    		public void onAccuracyChanged(Sensor sensor, int accuracy){
+    			
+    		}
+    	};
+    }
 }
