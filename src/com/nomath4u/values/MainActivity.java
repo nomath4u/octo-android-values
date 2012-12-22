f72645ef7309db2052d47a29446ea36f13529e24
Chris Harper <nomath4u@gmail.com>
2012-12-21 23:31:28 -0800

    Added orientation and list of available sensors, also a test of git

--- src/com/nomath4u/values/MainActivity.java
+++ src/com/nomath4u/values/MainActivity.java
@@ -1,5 +1,7 @@
 package com.nomath4u.values;
 
+import java.util.List;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
@@ -14,10 +16,16 @@ public class MainActivity extends Activity {
 	private SensorManager mSensorManager;
 	private Sensor mAccelerometer;
 	private Sensor mLightSensor;
+	private Sensor mOrientationSensor;
 	private SensorEventListener mAccelerometerListener;
 	private SensorEventListener mLightSensorListener;
+	private SensorEventListener mOrientationSensorListener;
 	private TextView mTextViewAccelerometer;
 	private TextView mTextViewLight;
+	private TextView mTextViewOrientation;
+	private TextView mTextViewSensors;
+	private List<Sensor> deviceSensors;
+	private String listOfSensors = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,15 +34,29 @@ public class MainActivity extends Activity {
         
         mTextViewAccelerometer = (TextView) findViewById(R.id.text_accelerometer);
         mTextViewLight = (TextView) findViewById(R.id.text_light);
+        mTextViewOrientation = (TextView) findViewById(R.id.text_orientation);
+        mTextViewSensors = (TextView) findViewById(R.id.text_sensors);
         
         initListeners();
+        
+       
+        	 
         
         // Get an instance of the SensorManager
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         mLightSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        mOrientationSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
         mSensorManager.registerListener(mAccelerometerListener, mAccelerometer,SensorManager.SENSOR_DELAY_UI);
         mSensorManager.registerListener(mLightSensorListener, mLightSensor, SensorManager.SENSOR_DELAY_UI);
+        mSensorManager.registerListener(mOrientationSensorListener, mOrientationSensor, SensorManager.SENSOR_DELAY_UI);
+        
+        //Get all Sensors and print them out
+        deviceSensors = mSensorManager.getSensorList(Sensor.TYPE_ALL);
+        for (Sensor sensor : deviceSensors){
+        	listOfSensors += (sensor.getName() + "\n");
+        }
+        mTextViewSensors.setText(listOfSensors);
     }
 
     @Override
@@ -62,6 +84,18 @@ public class MainActivity extends Activity {
     		public void onSensorChanged(SensorEvent event){
     			float[] values2 = event.values;
     			mTextViewLight.setText("Light Sensor: " + values2[0]);
+    		}
+    		
+    		@Override
+    		public void onAccuracyChanged(Sensor sensor, int accuracy){
+    		}
+    	};
+    	mOrientationSensorListener = new SensorEventListener(){
+    		@Override
+    		public void onSensorChanged(SensorEvent event){
+    			float[] value = event.values;
+    			//TO-DO add and describe other orientation sensor values
+    			mTextViewOrientation.setText("Orientation: " + value[0]);
     		}
     		
     		@Override
