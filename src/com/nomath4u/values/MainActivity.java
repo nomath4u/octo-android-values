ed2d50b5eae0d1435de65c809422977ef6d6e630
Chris Harper <nomath4u@gmail.com>
2012-12-24 23:09:25 -0800

    Code overhaul, now takes all available sensors and displays all available values from those sensors out to 5 decimal places. Layout does not scroll so not all values can been seen yet

--- src/com/nomath4u/values/MainActivity.java
+++ src/com/nomath4u/values/MainActivity.java
@@ -1,10 +1,13 @@
 package com.nomath4u.values;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -14,49 +17,35 @@ import android.hardware.SensorManager;
 public class MainActivity extends Activity {
 	
 	private SensorManager mSensorManager;
-	private Sensor mAccelerometer;
-	private Sensor mLightSensor;
-	private Sensor mOrientationSensor;
-	private SensorEventListener mAccelerometerListener;
-	private SensorEventListener mLightSensorListener;
-	private SensorEventListener mOrientationSensorListener;
-	private TextView mTextViewAccelerometer;
-	private TextView mTextViewLight;
-	private TextView mTextViewOrientation;
-	private TextView mTextViewSensors;
 	private List<Sensor> deviceSensors;
-	private String listOfSensors = "";
+	private ArrayList<SensorEventListener> mListOfSensorListeners = new ArrayList<SensorEventListener>();
+	private LinearLayout linearLayout;
+	private ArrayList<TextView> mTextViews = new ArrayList<TextView>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        mTextViewAccelerometer = (TextView) findViewById(R.id.text_accelerometer);
-        mTextViewLight = (TextView) findViewById(R.id.text_light);
-        mTextViewOrientation = (TextView) findViewById(R.id.text_orientation);
-        mTextViewSensors = (TextView) findViewById(R.id.text_sensors);
+       
+        linearLayout = (LinearLayout)findViewById(R.id.stuffz);
         
-        initListeners();
         
        
         	 
         
         // Get an instance of the SensorManager
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        mLightSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
-        mOrientationSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
-        mSensorManager.registerListener(mAccelerometerListener, mAccelerometer,SensorManager.SENSOR_DELAY_UI);
-        mSensorManager.registerListener(mLightSensorListener, mLightSensor, SensorManager.SENSOR_DELAY_UI);
-        mSensorManager.registerListener(mOrientationSensorListener, mOrientationSensor, SensorManager.SENSOR_DELAY_UI);
+      
         
-        //Get all Sensors and print them out
+        //Get all Sensors
         deviceSensors = mSensorManager.getSensorList(Sensor.TYPE_ALL);
-        for (Sensor sensor : deviceSensors){
-        	listOfSensors += (sensor.getName() + "\n");
-        }
-        mTextViewSensors.setText(listOfSensors);
+        
+        
+        initListeners();
+        registerViews();
+        
+        
     }
 
     @Override
@@ -67,41 +56,39 @@ public class MainActivity extends Activity {
     }
     
     private void initListeners(){
-    	mAccelerometerListener = new SensorEventListener(){
-    		@Override
-    		public void onSensorChanged(SensorEvent event){
-    			float[] values = event.values;
-    			mTextViewAccelerometer.setText("Accelerometer: " + values[0] + ", " + values [1] + ", " + values[2]);
-    		}
-    		
-    		@Override
-    		public void onAccuracyChanged(Sensor sensor, int accuracy){
-    			
-    		}
-    	};
-    	mLightSensorListener = new SensorEventListener(){
-    		@Override
-    		public void onSensorChanged(SensorEvent event){
-    			float[] values2 = event.values;
-    			mTextViewLight.setText("Light Sensor: " + values2[0]);
-    		}
-    		
-    		@Override
-    		public void onAccuracyChanged(Sensor sensor, int accuracy){
-    		}
-    	};
-    	mOrientationSensorListener = new SensorEventListener(){
-    		@Override
-    		public void onSensorChanged(SensorEvent event){
-    			float[] value = event.values;
-    			//TO-DO add and describe other orientation sensor values
-    			mTextViewOrientation.setText("Orientation: " + value[0]);
-    		}
-    		
-    		@Override
-    		public void onAccuracyChanged(Sensor sensor, int accuracy){
-    		}
-    	};
-    		    	
+    	//Fill List with Listeners
+    	
+    	for ( Sensor sensor : deviceSensors){
+    		final TextView tView = new TextView(this);
+    		 tView.setLayoutParams(new ViewGroup.LayoutParams(
+                     ViewGroup.LayoutParams.FILL_PARENT,
+                     ViewGroup.LayoutParams.WRAP_CONTENT));
+    		final Sensor thisSensor = sensor;
+    		SensorEventListener tmp = new SensorEventListener(){
+    			@Override
+    			public void onSensorChanged(SensorEvent event){
+    				float[] values = event.values;
+    				tView.setText(thisSensor.getName() + ": \n");
+    				for (float number : values){
+    					//Add each value to the text view
+    					tView.setText(tView.getText() + String.format("%.5g%n", number) + " ");
+    				}
+    			}
+    			@Override
+    			public void onAccuracyChanged(Sensor sensor, int accuracy){
+    				
+    			}
+    		};
+    	mSensorManager.registerListener(tmp,sensor,SensorManager.SENSOR_DELAY_UI);	
+    	mListOfSensorListeners.add(tmp);
+    	mTextViews.add(tView);
+    	
+    	}
+    }
+    
+    void registerViews(){
+    	for(TextView view : mTextViews){
+    		linearLayout.addView(view);
+    	}
     }
 }
